d55d65c4f91762b6afa3b81c6af002d52a610dfc
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-08-12 12:33:50 +0530

    initial, docker, submodules

---
+++ docker/start.sh
@@ -0,0 +1,13 @@
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+echo $BASEDIR
+cd $BASEDIR
+
+rm -r fe-build
+cd ../rm-comments-client
+
+# Build
+npm run build && mv build ../docker/fe-build
+
+cd $BASEDIR
+docker-compose up --build
